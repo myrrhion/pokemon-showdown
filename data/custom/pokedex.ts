067c6d1ab23ad2c63078bf8b8a9ce23316e81bd9
Myrrhion <gbuessin@students.uni-mainz.de>
2021-07-17 14:58:45 +0200

    Did an oopsie

--- data/custom/pokedex.ts
+++ data/custom/pokedex.ts
@@ -1,11 +1,3 @@
 export const Pokedex: {[k: string]: ModdedSpeciesData} = {
-	hitmonlee: {
-		inherit: true,
-		types: ["Electric"],
-		baseStats: {hp: 25, atk: 35, def: 70, spa: 95, spd: 95, spe: 45},
-	},
-	pichu: {
-		inherit: true,
-		evos: [],
-	},
+
 };
