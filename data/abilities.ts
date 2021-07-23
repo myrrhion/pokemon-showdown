aaffceaf15d713c4d820e155b8750d0707b3953e
Myrrhion <gbuessin@students.uni-mainz.de>
2021-07-23 11:17:56 +0200

    Custom Exclusive
    Moved all Custom Content to the Custom Folder.

--- data/abilities.ts
+++ data/abilities.ts
@@ -4487,38 +4487,4 @@ export const Abilities: {[abilityid: string]: AbilityData} = {
 		rating: 3,
 		num: -4,
 	},
-	infectiousbite: {
-		name: "Infectious Bite",
-		desc: "This is a long description",
-		shortDesc: "Infects Pokemon that attack it.",
-		onDamagingHit(damage, target, source, move) {
-			const sourceAbility = source.getAbility();
-			if (sourceAbility.isPermanent || sourceAbility.id === 'infectiousbite' || sourceAbility.id === 'infected') {
-				return;
-			}
-			if (this.checkMoveMakesContact(move, source, target, !source.isAlly(target))) {
-				const oldAbility = source.setAbility('infected', target);
-				if (oldAbility) {
-					this.add('-activate', target, 'ability: Infectious Bite', this.dex.abilities.get(oldAbility).name, '[of] ' + source);
-				}
-			}
-		},
-	},
-	infected: {
-		name: "Infected",
-		desc: "",
-		shortDesc: "Infects Pokemon that attack it, also, is effected.",
-		onDamagingHit(damage, target, source, move) {
-			const sourceAbility = source.getAbility();
-			if (sourceAbility.isPermanent || sourceAbility.id === 'infectiousbite') {
-				return;
-			}
-			if (this.checkMoveMakesContact(move, source, target, !source.isAlly(target))) {
-				const oldAbility = source.setAbility('mummy', target);
-				if (oldAbility) {
-					this.add('-activate', target, 'ability: Infected', this.dex.abilities.get(oldAbility).name, '[of] ' + source);
-				}
-			}
-		},
-	},
 };
